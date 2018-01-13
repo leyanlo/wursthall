d784063c1ea914613db85d42e40059b509d936c5
Toby Mao <toby.mao@gmail.com>
2018-01-13 00:06:59 -0800

    form

--- views/index.rb
+++ views/index.rb
@@ -15,7 +15,7 @@ module Views
 
         div class: 'right' do
           ul class: 'location' do
-             img src: '/images/icon-clock.png', class: 'clock_icon'
+            img src: '/images/icon-clock.png', class: 'clock_icon'
             li '11:00AM - 3:00 PM'
             li '310 Baldwin ave., San Mateo, CA 94401'
             li '650-305-1604'
@@ -40,46 +40,46 @@ module Views
 
       div class: 'second_section' do
         div class: 'second_section_content' do
-         div class: 'second_section_left' do
-          
-          img src: '/images/stamp-1.png', class: 'big_stamp'
+          div class: 'second_section_left' do
 
-          h2 "ABOUT US"
+            img src: '/images/stamp-1.png', class: 'big_stamp'
 
-          p do
-            text "Wursthall is a restaurant & bierhaus located in downtown San Mateo. We aim to bring the festivity and community of the German beerhall to the Bay Area, and hope to create a gathering space for friends and families."
-            br 
-            br
-            text "San Mateo natives, Adam Simpson and Tyson Mao, have partnered with James Beard award winning chef Kenji López-Alt to deliver a German and Austrian inspired menu for your enjoyment. Our menu features sausage plates, XXX (crispy fried potatoes), and more. We are proud to partner with Anne and Robert Moser of Backhaus Bakery for our pretzels and buns."
-            br
-            br
-            text  "We feature 22 beers which include German and Belgian style ales and local craft brews. We also feature a selection of red and white wine on tap."
-            br
-            br
-            text "Whether it’s an outing with some colleagues, a drink with an old friend, or a family night out for dinner, we hope you come and enjoy our contribution to downtown San Mateo. If you see us around, don’t hesitate to say hello!"
+            h2 "ABOUT US"
+
+            p do
+              text "Wursthall is a restaurant & bierhaus located in downtown San Mateo. We aim to bring the festivity and community of the German beerhall to the Bay Area, and hope to create a gathering space for friends and families."
+              br
+              br
+              text "San Mateo natives, Adam Simpson and Tyson Mao, have partnered with James Beard award winning chef Kenji López-Alt to deliver a German and Austrian inspired menu for your enjoyment. Our menu features sausage plates, XXX (crispy fried potatoes), and more. We are proud to partner with Anne and Robert Moser of Backhaus Bakery for our pretzels and buns."
+              br
+              br
+              text  "We feature 22 beers which include German and Belgian style ales and local craft brews. We also feature a selection of red and white wine on tap."
+              br
+              br
+              text "Whether it’s an outing with some colleagues, a drink with an old friend, or a family night out for dinner, we hope you come and enjoy our contribution to downtown San Mateo. If you see us around, don’t hesitate to say hello!"
+            end
+
+
+            img src: '/images/stamp-2.png', class: 'small_stamp'
           end
 
-
-         img src: '/images/stamp-2.png', class: 'small_stamp'
+          div class: 'second_section_right' do
+            img src: '/images/aboutus-imgs.png', class: 'aboutus_image'
+          end
         end
-
-        div class: 'second_section_right' do
-          img src: '/images/aboutus-imgs.png', class: 'aboutus_image'
-        end
-       end 
       end
 
 
-        
+
 
       div class: 'third_section' do
-                div class: 'third_section_content' do
+        div class: 'third_section_content' do
 
           h2 "MENU"
 
           p do
             text "Lorem ipsum dolor sit amet, consul gloriatur at mel. At omnium expetendis pro. Persius detracto concludaturque id eos, nam alia euismod fabellas te, sed unum minimum lobortis eu. Utinam theophrastus no mel, nisl possit ne his, id dolor pertinacia his. Nulla scriptorem no mel. Volumus lobortis urbanitas mea te."
-            br 
+            br
             br
             text "Eu vis saperet oportere, labore putant molestiae his cu. Animal intellegat eu cum, postea cetero salutandi ad cum, lorem munere eos at. At nam veri melius, mea eu minimum delicatissimi. Id simul accusam phaedrum sed, tibique similique eu nam. An vel nobis eligendi vituperata. Sit ad vocent placerat honestatis, eu qui eirmod voluptatibus. Cu inermis accumsan duo, regione aliquid in vix. Download Menu"
           end
@@ -92,7 +92,7 @@ module Views
             img src: '/images/home_menu_button_latenight-off@2x.jpg', class: 'menu_latenight'
             img src: '/images/home_menu_button_drinks-off@2x.jpg', class: 'menu_drinks'
           end
-       end   
+        end
       end
 
 
@@ -103,15 +103,23 @@ module Views
           h2 "CONTACT US"
           p 'Have a question or a comment? Let us know how we can help.'
 
-            div class: 'fourth_section_contact' do
-
-              p 'form'
-              p 'map'
-
-            end
+          render_form
+          render_map
         end
       end
 
+    end
+
+    def render_form
+      form method: 'post' do
+        input type: 'text', name: 'name', placeholder: 'Name'
+        input type: 'text', name: 'email', placeholder: 'E-Mail'
+        input type: 'textarea', name: 'description', placeholder: 'TELL US ABOUT YOUR EVENT...'
+      end
+    end
+
+    def render_map
+      iframe width: "600", height:"450", frameborder:"0", style:"border:0", src: "https://www.google.com/maps/embed/v1/place?q=310%20Baldwin%20ave.%2C%20San%20Mateo%2C%20CA%2094401&key=AIzaSyD8Lt6KLOwOoruU-AO70HsZ8a4t_9zqmPY"
     end
 
     def render_footer
